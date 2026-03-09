11cdfdb09056d86c6f219b759b624684ca599edf
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2026-03-09 09:00:50 +0100

    Selenium + Selenide UI testing

--- FirstSemester/src/main/java/org/example/Main.java
+++ FirstSemester/src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.lesson05.web.Lesson05WebServer;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Lesson05WebServer.main(new String[0]);
+    public static void main(String[] args) {
     }
 }
--- FirstSemester/src/main/java/org/example/lesson05/Grades.java
+++ FirstSemester/src/main/java/org/example/lesson05/Grades.java
@@ -1,4 +1,9 @@
 package org.example.lesson05;
 
 public class Grades {
+
+    public void convert(String gradeToConvert, String countryGradeBelongsTo) {
+
+
+    }
 }
